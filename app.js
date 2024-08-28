af41e93b463d6d8cb82458b659a673f45532156d
Ibrahim Ahmed <Ibrahimahmed2702@gmail.com>
2024-08-28 14:41:24 +0100

    completed step 7 of posting a comment on the article requested

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 const express = require("express");
+const app = express();
+app.use(express.json());
+
 const { getTopics } = require("./controllers/topics-controllers");
 const { getEndpoints } = require("./controllers/endpoint-controllers")
 const { getArticleFromId, getArticles} = require("./controllers/articles-controllers")
-const app = express();
 const { endpointErrorHandler, serverErrorHandler, psqlErrorHandler } = require("./error-handlers")
-const { getArticleComments } = require("./controllers/comments-controllers")
+const { getArticleComments, postCommentOnArticle } = require("./controllers/comments-controllers")
 
 app.get("/api/topics", getTopics);
 app.get("/api", getEndpoints);
 app.get("/api/articles/:article_id", getArticleFromId);
-app.get("/api/articles", getArticles)
-app.get("/api/articles/:article_id/comments", getArticleComments)
+app.get("/api/articles", getArticles);
+app.get("/api/articles/:article_id/comments", getArticleComments);
+
+app.post("/api/articles/:article_id/comments", postCommentOnArticle);
 
 app.all("*", endpointErrorHandler);
 
 app.use(psqlErrorHandler);
 app.use(serverErrorHandler);
-
 
 module.exports = app;
